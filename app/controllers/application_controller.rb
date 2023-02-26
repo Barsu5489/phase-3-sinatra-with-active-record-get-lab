83f7b61598390b5698e435a5d5b76cb913ad101f
barsu5489 <ebarsulai@gmail.com>
2023-02-26 23:57:10 +0300

    active record GET

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  before do
+    content_type :json
+  end
 
+  # add routes
+  get '/bakeries' do
+    bake = Bakery.all
+    bake.to_json
+  end
+
+  get '/bakeries/:id' do
+    good = Bakery.find(params[:id])
+    good.to_json(include: :baked_goods)
+  end
+get '/baked_goods/by_price' do
+  price = BakedGood.all.order(price: :desc)
+  price.to_json
+end
+get '/baked_goods/most_expensive' do
+ price =  BakedGood.all.order(price: :desc).first
+ price.to_json
+end
 end
